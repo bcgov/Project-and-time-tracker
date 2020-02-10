9f87fdba736f65a83bc6a74edabbd518a425088c
Adam Coard <arcoard+github@gmail.com>
2020-02-10 12:58:23 -0800

    Update vue.config baseUrl

--- web/vue.config.js
+++ web/vue.config.js
@@ -2,4 +2,9 @@ module.exports = {
   configureWebpack: {
     devtool: 'source-map',
   },
+
+  // NOTE - ONLY FOR GitHub Pages! Any way to easily configure, maybe env var
+  // Using ./ for GH pages - need to verify but turns paths relative
+  // https://github.com/vuejs/vue-cli/issues/1623
+  baseUrl: './'
 };
