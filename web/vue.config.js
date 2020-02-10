a754268983d08cc1cd611b32bdbe148aa22435bf
Adam Coard <arcoard+github@gmail.com>
2020-02-10 13:02:03 -0800

    Updating baseUrl again

--- web/vue.config.js
+++ web/vue.config.js
@@ -6,5 +6,6 @@ module.exports = {
   // NOTE - ONLY FOR GitHub Pages! Any way to easily configure, maybe env var
   // Using ./ for GH pages - need to verify but turns paths relative
   // https://github.com/vuejs/vue-cli/issues/1623
-  baseUrl: './'
+  // baseUrl: './'
+  baseUrl: '/Project-and-time-tracker/'
 };
