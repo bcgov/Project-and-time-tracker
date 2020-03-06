8b3c64481896b3d87b32780a2e173142126e4f9b
Adam Coard <arcoard+github@gmail.com>
2020-03-06 09:47:48 -0800

    Fix keycloak admin key issue

--- api/src/app/config/env-config.ts
+++ api/src/app/config/env-config.ts
@@ -3,11 +3,6 @@ export  const ENV_CONFIG =  {
         'database': {
             'postgresdb': {
                 'type': 'postgres',
-                // 'host': 'timemachinedev.cu9f5va0d5zo.us-east-1.rds.amazonaws.com',
-                // 'port': 5432,
-                // 'username': 'timeTrackerAdmin',
-                // 'password': 'T1meTrack3r#567',
-                // 'database': 'timeMachineDev',
                 'host': 'localhost',
                 'port': 5432,
                 'username': 'postgres',
@@ -20,9 +15,8 @@ export  const ENV_CONFIG =  {
             realm: 'Time_Tracker_dev',
             resource: 'timeMachineApp',
             authServerUrl: 'http://iam.aot-technologies.com/auth',
-            adminUserName: 'admin',
+            adminUserName: 'dev_admin',
             adminPassword: 'aot123'
         }
     },
 };
-
