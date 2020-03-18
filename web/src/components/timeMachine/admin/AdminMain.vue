710307f3ef29725c1df8ce245a724baebd68d818
Adam Coard <arcoard+github@gmail.com>
2020-03-18 16:31:04 -0700

    Improve UI of admin screen

--- web/src/components/timeMachine/admin/AdminMain.vue
+++ web/src/components/timeMachine/admin/AdminMain.vue
@@ -7,31 +7,46 @@
     </v-layout>
     <v-layout row wrap>
       <v-flex xs12>
-        <a v-bind:href='AUTH_URL'>Keycloak - IAM</a>
+        <v-list two-line subheader>
+          <v-subheader>Admininistrator Controls</v-subheader>
 
-        <br>
-        <!-- <a href="">Ministries</a> -->
-        <router-link to="admin/ministries">Ministries</router-link>
+          <v-list-tile avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                <router-link to="admin/ministries">Ministries</router-link>
+              </v-list-tile-title>
+              <v-list-tile-sub-title>Add or archive ministries</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
 
+          <v-list-tile avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                <a v-bind:href="AUTH_URL">Keycloak - IAM</a>
+              </v-list-tile-title>
+              <v-list-tile-sub-title>Login to Keycloak - to manage users.</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-import Material from 'vuetify/es5/util/colors';
+import Material from "vuetify/es5/util/colors";
 // web/src/modules/security/init.js
-import { AUTH_URL, getAuthURL } from '../../../modules/security/init';
+import { AUTH_URL, getAuthURL } from "../../../modules/security/init";
 
 export default {
   data: () => ({
     color: Material,
-    selectedTab: 'tab-1',
-    AUTH_URL: getAuthURL() + '/admin'
+    selectedTab: "tab-1",
+    AUTH_URL: getAuthURL() + "/admin"
   }),
   beforeMount() {
     // this.$store.state.collapseNavigationBar = true;
-  },
+  }
 };
 </script>
 
