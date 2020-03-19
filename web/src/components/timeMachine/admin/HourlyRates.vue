f53dcf3e204f34f32df0fd1d0f269e89e0c6ef58
Adam Coard <arcoard+github@gmail.com>
2020-03-19 12:46:19 -0700

    Improve hourly rates formatting and search

--- web/src/components/timeMachine/admin/HourlyRates.vue
+++ web/src/components/timeMachine/admin/HourlyRates.vue
@@ -6,32 +6,53 @@
         <h1 class="projects-header">Admin &mdash; Hourly Rates</h1>
       </v-flex>
     </v-layout>
+    <v-layout row>
+      <v-flex sm6 offset-sm6>
+        <v-text-field
+          class="search-bar"
+          v-model="search"
+          append-icon="search"
+          label="Search Project"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
     <v-layout row wrap>
-      <v-flex xs12>
-        <v-layout>
-          <v-data-table :headers="headers" :items="users" class="elevation-1">
-            <template v-slot:items="props">
-              <td>{{ props.item.contact.fullName }}</td>
-              <td>
-                <v-edit-dialog
-                  :return-value.sync="props.item.contact.hourlyRate"
-                  lazy
-                  @save="save(props.item.contact)"
-                  @cancel="cancel"
-                  @open="open"
-                  @close="close"
+      <v-flex>
+        <v-data-table
+          :headers="headers"
+          :items="users"
+          hide-actions
+          class="elevation-0 tm-v-datatable"
+        >
+          <template v-slot:items="props">
+            <td>{{ props.item.contact.fullName }}</td>
+            <td>
+              <v-edit-dialog
+                :return-value.sync="props.item.contact.hourlyRate"
+                lazy
+                @save="save(props.item.contact)"
+                @cancel="cancel"
+                @open="open"
+                @close="close"
+              >
+                {{ props.item.contact.hourlyRate ? `$${props.item.contact.hourlyRate}/hr` : 'n/a' }}
+                <template
+                  v-slot:input
                 >
-                  {{ props.item.contact.hourlyRate ? props.item.contact.hourlyRate : 'none set' }}
-                  <template
-                    v-slot:input
-                  >
-                    <v-text-field v-model="props.item.contact.hourlyRate" label="Rate" single-line type="number"></v-text-field>
-                  </template>
-                </v-edit-dialog>
-              </td>
-            </template>
-          </v-data-table>
-        </v-layout>
+                  <v-text-field
+                    v-model="props.item.contact.hourlyRate"
+                    label="Rate"
+                    single-line
+                    type="number"
+                  ></v-text-field>
+                </template>
+              </v-edit-dialog>
+            </td>
+          </template>
+        </v-data-table>
+        <!-- </v-layout> -->
       </v-flex>
     </v-layout>
   </v-container>
@@ -49,25 +70,27 @@ export default {
       headers: [
         { text: "Name", value: "name" },
         { text: "Rate", value: "rate" }
-      ]
+      ],
+      search: ""
     };
   },
   computed: {
     users() {
+      if (this.search) {
+        return this.$store.state.users.filter(item => {
+          return item.contact.fullName
+            .toLowerCase()
+            .includes(this.search.toLowerCase());
+        });
+      }
       return this.$store.state.users;
-      // return [];
     }
-    // userList() {
-    //   return this.$store.state.users;
-    // }
   },
   methods: {
     fetchData() {
-      console.log("fetch");
       this.$store.dispatch("fetchUsers");
     },
     async save(contact) {
-      console.log("save", { contact });
       await this.$store.dispatch("updateContactPartial", {
         id: contact.id,
         hourlyRate: contact.hourlyRate
@@ -77,15 +100,8 @@ export default {
     cancel() {
       this.$refs.snackbar.displaySnackbar("info", "Cancelled");
     },
-    open() {
-      console.log("opened");
-    },
-    close(x) {
-      console.log("Dialog closed, update user", x);
-      // const body = {id, hourlyRate}
-      // can we update CONTACT via the USER service???
-      // this.$store.dispatch('updateContactPartial', )
-    }
+    open() {},
+    close() {}
   },
   created() {
     this.fetchData();
