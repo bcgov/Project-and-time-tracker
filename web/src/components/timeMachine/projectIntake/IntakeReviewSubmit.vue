4695ce4acd8dc6519187cef7e3bf14ca88b3673a
arungmani-aot <arungmani@aot-technologies.com>
2020-04-01 11:56:33 +0530

    mask phone number issue is fixed

--- web/src/components/timeMachine/projectIntake/IntakeReviewSubmit.vue
+++ web/src/components/timeMachine/projectIntake/IntakeReviewSubmit.vue
@@ -9,47 +9,65 @@
           <v-card-text class="pl-4">
             <v-flex>
               <v-btn color="primary" @click="editItem(1)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>Project Name</h4></v-flex>
+              <v-flex sm12>
+                <h4>Project Name</h4>
+              </v-flex>
               <v-flex sm12>{{ $props.intakeValues.projectName }}</v-flex>
             </v-flex>
             <v-flex>
               <v-btn color="primary" @click="editItem(1)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>Project Sector</h4></v-flex>
+              <v-flex sm12>
+                <h4>Project Sector</h4>
+              </v-flex>
               <v-flex sm12>{{ getProjectSector() }}</v-flex>
             </v-flex>
             <v-flex>
               <v-btn color="primary" @click="editItem(1)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>Desired Date of Completion</h4></v-flex>
+              <v-flex sm12>
+                <h4>Desired Date of Completion</h4>
+              </v-flex>
               <v-flex sm12>{{ $props.intakeValues.estimatedCompletionDate }}</v-flex>
             </v-flex>
             <v-flex>
               <v-btn color="primary" @click="editItem(1)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>Contract Amount</h4></v-flex>
+              <v-flex sm12>
+                <h4>Contract Amount</h4>
+              </v-flex>
               <v-flex sm12>{{ $props.intakeValues.estimatedContractValue }}</v-flex>
             </v-flex>
             <v-flex>
               <v-btn color="primary" @click="editItem(1)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>Reprocurement?</h4></v-flex>
-              <v-flex sm12> {{ checkBoolenValues($props.intakeValues.isReprocurement) }}</v-flex>
+              <v-flex sm12>
+                <h4>Reprocurement?</h4>
+              </v-flex>
+              <v-flex sm12>{{ checkBoolenValues($props.intakeValues.isReprocurement) }}</v-flex>
             </v-flex>
             <v-flex v-if="$props.intakeValues.isReprocurement">
               <v-btn color="primary" @click="editItem(1)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>Date original contract ends</h4></v-flex>
+              <v-flex sm12>
+                <h4>Date original contract ends</h4>
+              </v-flex>
               <v-flex sm12>{{ $props.intakeValues.dateOfReprocurement }}</v-flex>
             </v-flex>
             <v-flex v-if="$props.intakeValues.isReprocurement">
               <v-btn color="primary" @click="editItem(1)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>What is the background from the previous contract?</h4></v-flex>
+              <v-flex sm12>
+                <h4>What is the background from the previous contract?</h4>
+              </v-flex>
               <v-flex sm12>{{ $props.intakeValues.previousContractBackground }}</v-flex>
             </v-flex>
             <v-flex>
               <v-btn color="primary" @click="editItem(1)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>What is the potential impact of this procurement project failing?</h4></v-flex>
+              <v-flex sm12>
+                <h4>What is the potential impact of this procurement project failing?</h4>
+              </v-flex>
               <v-flex sm12>{{ $props.intakeValues.projectFailImpact }}</v-flex>
             </v-flex>
             <v-flex>
               <v-btn color="primary" @click="editItem(1)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>What does success look like for this procurement project?</h4></v-flex>
+              <v-flex sm12>
+                <h4>What does success look like for this procurement project?</h4>
+              </v-flex>
               <v-flex sm12>{{ $props.intakeValues.projectSuccess }}</v-flex>
             </v-flex>
           </v-card-text>
@@ -63,69 +81,95 @@
           <v-card-text class="pl-4" v-if="$props.intakeValues.client">
             <v-flex>
               <v-btn color="primary" @click="editItem(2)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>Non-Ministry</h4></v-flex>
-              <v-flex sm12>{{
+              <v-flex sm12>
+                <h4>Non-Ministry</h4>
+              </v-flex>
+              <v-flex sm12>
+                {{
                 checkBoolenValues($props.intakeValues.client.isNonMinistry)
-              }}</v-flex>
+                }}
+              </v-flex>
             </v-flex>
             <v-flex>
               <v-btn color="primary" @click="editItem(2)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>Ministry</h4></v-flex>
+              <v-flex sm12>
+                <h4>Ministry</h4>
+              </v-flex>
               <v-flex sm12>{{ getMinistry() }}</v-flex>
             </v-flex>
             <v-flex>
               <v-btn color="primary" @click="editItem(2)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>Branch</h4></v-flex>
+              <v-flex sm12>
+                <h4>Branch</h4>
+              </v-flex>
               <v-flex sm12>{{ $props.intakeValues.client.orgDivision }}</v-flex>
             </v-flex>
             <v-flex>
               <v-btn color="primary" @click="editItem(2)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>Address Line 1</h4></v-flex>
+              <v-flex sm12>
+                <h4>Address Line 1</h4>
+              </v-flex>
               <v-flex sm12>{{ $props.intakeValues.client.addressLine1 }}</v-flex>
             </v-flex>
             <v-flex>
               <v-btn color="primary" @click="editItem(2)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>Address Line 2</h4></v-flex>
+              <v-flex sm12>
+                <h4>Address Line 2</h4>
+              </v-flex>
               <v-flex sm12>{{ $props.intakeValues.client.addressLine2 }}</v-flex>
             </v-flex>
             <v-flex>
               <v-btn color="primary" @click="editItem(2)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>City</h4></v-flex>
+              <v-flex sm12>
+                <h4>City</h4>
+              </v-flex>
               <v-flex sm12>{{ $props.intakeValues.client.city }}</v-flex>
             </v-flex>
             <v-flex>
               <v-btn color="primary" @click="editItem(2)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>Postal</h4></v-flex>
+              <v-flex sm12>
+                <h4>Postal</h4>
+              </v-flex>
               <v-flex sm12>{{ $props.intakeValues.client.postalCode }}</v-flex>
             </v-flex>
 
             <v-flex>
               <v-btn color="primary" @click="editItem(2)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>Client No.</h4></v-flex>
+              <v-flex sm12>
+                <h4>Client No.</h4>
+              </v-flex>
               <v-flex sm12>{{ $props.intakeValues.client.clientNo }}</v-flex>
             </v-flex>
 
             <v-flex>
               <v-btn color="primary" @click="editItem(2)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>Responsibility Center</h4></v-flex>
+              <v-flex sm12>
+                <h4>Responsibility Center</h4>
+              </v-flex>
               <v-flex sm12>{{ $props.intakeValues.client.responsibilityCenter }}</v-flex>
             </v-flex>
 
             <v-flex>
               <v-btn color="primary" @click="editItem(2)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>Service Center</h4></v-flex>
+              <v-flex sm12>
+                <h4>Service Center</h4>
+              </v-flex>
               <v-flex sm12>{{ $props.intakeValues.client.serviceCenter }}</v-flex>
             </v-flex>
 
             <v-flex>
               <v-btn color="primary" @click="editItem(2)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>STOB</h4></v-flex>
+              <v-flex sm12>
+                <h4>STOB</h4>
+              </v-flex>
               <v-flex sm12>{{ $props.intakeValues.client.stob }}</v-flex>
             </v-flex>
 
             <v-flex>
               <v-btn color="primary" @click="editItem(2)" class="edit-link">edit</v-btn>
-              <v-flex sm12><h4>Project Code</h4></v-flex>
+              <v-flex sm12>
+                <h4>Project Code</h4>
+              </v-flex>
               <v-flex sm12>{{ $props.intakeValues.client.projectCode }}</v-flex>
             </v-flex>
           </v-card-text>
@@ -139,9 +183,11 @@
           <v-card-text class="pl-4" v-if="$props.intakeValues.risk">
             <v-flex v-for="item in $props.intakeValues.risk" :key="item.id">
               <v-btn color="primary" @click="editItem(3)" class="edit-link">edit</v-btn>
-              <v-flex sm12
-                ><b><div v-html="item.question" class="question-text"></div></b
-              ></v-flex>
+              <v-flex sm12>
+                <b>
+                  <div v-html="item.question" class="question-text"></div>
+                </b>
+              </v-flex>
               <v-flex sm12>{{ item.answer }}</v-flex>
             </v-flex>
           </v-card-text>
@@ -161,48 +207,60 @@
               <h3 v-if="contactItem.fullName">{{ getContactType(contactItem.contactType) }}</h3>
               <v-flex v-if="contactItem.fullName">
                 <v-btn color="primary" @click="editItem(4)" class="edit-link">edit</v-btn>
-                <v-flex sm12><h4>Name</h4></v-flex>
+                <v-flex sm12>
+                  <h4>Name</h4>
+                </v-flex>
                 <v-flex sm12>{{ contactItem.fullName }}</v-flex>
               </v-flex>
 
               <v-flex v-if="contactItem.email">
                 <v-btn color="primary" @click="editItem(4)" class="edit-link">edit</v-btn>
-                <v-flex sm12><h4>Email Address</h4></v-flex>
+                <v-flex sm12>
+                  <h4>Email Address</h4>
+                </v-flex>
                 <v-flex sm12>{{ contactItem.email }}</v-flex>
               </v-flex>
 
               <v-flex v-if="contactItem.orgName">
                 <v-btn color="primary" @click="editItem(4)" class="edit-link">edit</v-btn>
-                <v-flex sm12><h4>Organization</h4></v-flex>
+                <v-flex sm12>
+                  <h4>Organization</h4>
+                </v-flex>
                 <v-flex sm12>{{ contactItem.orgName }}</v-flex>
               </v-flex>
 
               <v-flex v-if="contactItem.orgPosition">
                 <v-btn color="primary" @click="editItem(4)" class="edit-link">edit</v-btn>
-                <v-flex sm12><h4>Position Title</h4></v-flex>
+                <v-flex sm12>
+                  <h4>Position Title</h4>
+                </v-flex>
                 <v-flex sm12>{{ contactItem.orgPosition }}</v-flex>
               </v-flex>
 
               <v-flex v-if="contactItem.phoneNumber">
                 <v-btn color="primary" @click="editItem(4)" class="edit-link">edit</v-btn>
-                <v-flex sm12><h4>Phone Number</h4></v-flex>
-                <v-flex sm12>{{ contactItem.phoneNumber }}</v-flex>
+                <v-flex sm12>
+                  <h4>Phone Number</h4>
+                </v-flex>
+                <v-flex sm12>{{ maskPhone(contactItem.phoneNumber) }}</v-flex>
               </v-flex>
-          </v-flex>
+            </v-flex>
           </v-card-text>
         </v-card>
       </v-expansion-panel-content>
-      <v-btn flat icon @click="print()"><v-icon>print</v-icon>Print</v-btn>
+      <v-btn flat icon @click="print()">
+        <v-icon>print</v-icon>Print
+      </v-btn>
     </v-expansion-panel>
   </div>
 </template>
 <script>
-import './intakeReviewSubmit.styl';
+import "./intakeReviewSubmit.styl";
 
 export default {
   components: {},
   props: {
-    intakeValues: {},
+    intakeValues: {}
   },
   computed: {},
 
@@ -211,41 +269,48 @@ export default {
     const form = Object.assign({}, this.$props.intakeValues);
     return {
       form: { ...form },
-      panelState: [true],
+      panelState: [true]
     };
   },
   watch: {},
 
   methods: {
+    maskPhone(e) {
+      var x = e.replace(/\D/g, "").match(/(\d{3})(\d{3})(\d{4})/);
+     return x = "(" + x[1] + ") " + x[2] + "-" + x[3];
+    },
     editItem(itemIndex) {
-      this.$emit('switch', itemIndex);
+      this.$emit("switch", itemIndex);
     },
     checkBoolenValues(value) {
       if (value) {
-        return 'Yes';
+        return "Yes";
       }
-      return 'No';
+      return "No";
     },
     getContactType(contactType) {
-      if (contactType === 'clientfinance') {
-        return 'Client Finance';
+      if (contactType === "clientfinance") {
+        return "Client Finance";
       }
-      if (contactType === 'clientlead') {
-        return 'Client Lead';
+      if (contactType === "clientlead") {
+        return "Client Lead";
       }
-      if (contactType === 'clientsponsor') {
-        return 'Client Sponsor';
+      if (contactType === "clientsponsor") {
+        return "Client Sponsor";
       }
-      if (contactType === 'clientcontact') {
-        return 'Client Contact';
+      if (contactType === "clientcontact") {
+        return "Client Contact";
       }
-      return '';
+      return "";
     },
     getProjectSector() {
-      const projectSector = Object.assign({}, this.$props.intakeValues.projectSector);
+      const projectSector = Object.assign(
+        {},
+        this.$props.intakeValues.projectSector
+      );
       if (projectSector.id) {
         const sector = this.$store.state.projectSectors.filter(
-          item => item.id === projectSector.id,
+          item => item.id === projectSector.id
         );
         if (sector[0]) {
           return sector[0].projectSectorName;
@@ -253,24 +318,27 @@ export default {
       } else if (this.$props.intakeValues.otherProjectSectorName) {
         return this.$props.intakeValues.otherProjectSectorName;
       }
-      return '';
+      return "";
     },
     getMinistry() {
       if (this.$props.intakeValues.client.isNonMinistry) {
         return this.$props.intakeValues.client.nonMinistryName
           ? this.$props.intakeValues.client.nonMinistryName
-          : '';
+          : "";
       }
-      const ministry = Object.assign({}, this.$props.intakeValues.client.ministry);
+      const ministry = Object.assign(
+        {},
+        this.$props.intakeValues.client.ministry
+      );
       if (ministry.id) {
         const selectedMinistry = this.$store.state.ministries.filter(
-          item => item.id === ministry.id,
+          item => item.id === ministry.id
         );
         if (selectedMinistry[0]) {
           return selectedMinistry[0].ministryName;
         }
       }
-      return '';
+      return "";
     },
     print() {
       this.expandAll();
@@ -278,8 +346,8 @@ export default {
     },
     expandAll() {
       this.panelState = [true, true, true, true, true];
-    },
-  },
+    }
+  }
 };
 </script>
 <style scoped>
