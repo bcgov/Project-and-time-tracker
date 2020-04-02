f1f66a14d4e523a8ff1fcc9a9aa157e051a8913a
Adam Coard <arcoard+github@gmail.com>
2020-04-02 13:12:34 -0700

    Manually fix merge conflict with All / Other projects

--- web/src/components/timeMachine/projects/Projects.vue
+++ web/src/components/timeMachine/projects/Projects.vue
@@ -3,15 +3,15 @@
       <v-flex md12>
         <v-radio-group v-model="selected" row>
         <v-radio label="My Projects" value="My Projects"></v-radio>
-        <v-radio label="Other Projects" value="Other Projects"></v-radio>
+        <v-radio label="All Projects" value="All Projects"></v-radio>
         <v-radio label="Archived Projects" value="Archived Projects"></v-radio>
       </v-radio-group>
       </v-flex>
       <h1 class="projects-header">View {{ selected }}</h1>
       <v-layout row wrap>
       <v-flex xs12>
-        <projects-table v-if="selected === 'My Projects'" :selectedItem="2"></projects-table>
-        <projects-table v-if="selected === 'Other Projects'" :selectedItem="1"></projects-table>
+        <projects-table v-if="selected === 'My Projects'"></projects-table>
+        <projects-table v-if="selected === 'All Projects'"></projects-table>
         <archived-projects-table v-if="selected === 'Archived Projects'"></archived-projects-table>
       </v-flex>
     </v-layout>
