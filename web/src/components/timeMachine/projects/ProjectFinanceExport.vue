6b7c77c2105cf638185fee30b0949d44fa09edea
Sonia Jacob <sonia.pm@aot-technologies.com>
2020-04-19 03:24:21 +0530

    html export to pdf

--- web/src/components/timeMachine/projects/ProjectFinanceExport.vue
+++ web/src/components/timeMachine/projects/ProjectFinanceExport.vue
@@ -30,7 +30,7 @@
     </v-flex>
     <v-flex md-10 >
       <div class="start-button-div">
-      <v-btn color="primary" class="start-button-style" @click="exportToPDF">Export Finance to PDF</v-btn></div>
+      <v-btn color="primary" class="start-button-style" @click="demoFromHTML">Export Finance to PDF</v-btn></div>
     </v-flex></v-layout>
     <v-layout row wrap>
       <v-flex xs12>
@@ -129,6 +129,47 @@ export default {
       doc.text('page 2', leftStartCoordinate, topStartCoordinate);
 
       doc.save('sample.pdf');
+    },
+
+    demoFromHTML() {
+      const pdf = new jsPDF('p', 'pt', 'letter');
+      // source can be HTML-formatted string, or a reference
+      // to an actual DOM element from which the text will be scraped.
+      const source = '<body>The content of the document......</body>';
+
+      // we support special element handlers. Register them with jQuery-style
+      // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+      // There is no support for any other type of selectors
+      // (class, of compound) at this time.
+      const specialElementHandlers = {
+        // element with id of "bypass" - jQuery style selector
+        '#bypassme': function (element, renderer) {
+          // true = "handled elsewhere, bypass text extraction"
+          return true;
+        },
+      };
+      const margins = {
+        top: 80,
+        bottom: 60,
+        left: 40,
+        width: 522,
+      };
+      // all coords and widths are in jsPDF instance's declared units
+      // 'inches' in this case
+      pdf.fromHTML(
+        source, // HTML string or DOM elem ref.
+        margins.left, // x coord
+        margins.top, { // y coord
+          width: margins.width, // max width of content on PDF
+          elementHandlers: specialElementHandlers,
+        },
+
+        (dispose) => {
+          // dispose: object with X, Y of the last line add to the PDF
+          //          this allow the insertion of new lines after html
+          pdf.save('Test.pdf');
+        }, margins,
+      );
     },
   },
 
