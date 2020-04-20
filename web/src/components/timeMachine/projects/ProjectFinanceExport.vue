916c8d660bad8484f9a9805f67b23a1020345fd2
Sonia Jacob <sonia.pm@aot-technologies.com>
2020-04-21 02:38:10 +0530

    populated JSON data to PDF

--- web/src/components/timeMachine/projects/ProjectFinanceExport.vue
+++ web/src/components/timeMachine/projects/ProjectFinanceExport.vue
@@ -22,7 +22,7 @@
             ></v-text-field>
           </template>
           <v-date-picker v-model="date" :show-current="true" type="month">
-               <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
             <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
             <v-btn text color="primary" @click="datefilter(date)">OK</v-btn>
           </v-date-picker>
@@ -30,11 +30,9 @@
       </v-flex>
       <v-flex md-10>
         <div class="start-button-div">
-          <v-btn
-            color="primary"
-            class="start-button-style"
-            @click="exportToPDF"
-          >Export Finance to PDF</v-btn>
+          <v-btn color="primary" class="start-button-style" @click="exportToPDF"
+            >Export Finance to PDF</v-btn
+          >
         </div>
       </v-flex>
     </v-layout>
@@ -48,28 +46,28 @@
 </template>
 
 <script>
-import Material from "vuetify/es5/util/colors";
-import jsPDF from "jspdf";
-import ProjectsTable from "./ProjectTableExport.vue";
-import "jspdf-autotable";
-import "./projects.styl";
+import Material from 'vuetify/es5/util/colors';
+import jsPDF from 'jspdf';
+import ProjectsTable from './ProjectTableExport.vue';
+import 'jspdf-autotable';
+import './projects.styl';
 
 export default {
   components: {
-    ProjectsTable
+    ProjectsTable,
   },
   data: () => ({
     color: Material,
-    selected: "My Projects",
+    selected: 'My Projects',
     date: new Date().toISOString().substr(0, 7),
     menu: false,
-    modal: false
+    modal: false,
   }),
   computed: {},
   methods: {
     datefilter(date) {
-    this.$refs.menu.save(date);
-    this.$refs.projectFinanceExport.getAllProjectList(date);
+      this.$refs.menu.save(date);
+      this.$refs.projectFinanceExport.getAllProjectList(date);
     },
     dataForPdfCreation() {
       return {};
@@ -78,31 +76,37 @@ export default {
       let projects = this.$refs.projectFinanceExport.getAllProjectIds();
       const vm = this;
       projects = projects.map(str => ({ projectId: str }));
-      let pdfValues;
-      vm.$store.dispatch("financeExport", projects).then(() => {
-        pdfValues = vm.$store.state.financeExport;
+      const pdfValues = [];
+      vm.$store.dispatch('financeExport', projects).then(() => {
+        debugger;
+        vm.$store.state.financeExport.forEach((entry) => {
+          const exportData = JSON.parse(entry.exportData);
+          pdfValues.push(exportData);
+        });
+        if (pdfValues.length === 0) {
+          return;
+        }
 
-        /////////////// PDF FIRST PAGE START /////////////////////////////////////////////
+        // ///////////// PDF FIRST PAGE START /////////////////////////////////////////////
         const doc = new jsPDF({
           putOnlyUsedFonts: true,
-          orientation: "portrait"
+          orientation: 'portrait',
         });
         const tableHeaders = [
-          "Client",
-          "Responsibility",
-          "Service Line",
-          "STOB",
-          "Project",
-          "Amount"
+          'Client',
+          'Responsibility',
+          'Service Line',
+          'STOB',
+          'Project',
+          'Amount',
         ];
         const tableRowsFormatted = pdfValues.map(proj => [
-          proj.clientNo,
-          proj.responsibilityCenter,
-          proj.serviceCenter,
-          proj.stob,
-          proj.projectCode,
-          "$" +
-            proj.totalAmount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+          proj.clientNo ? proj.clientNo : '',
+          proj.responsibilityCenter ? proj.responsibilityCenter : '',
+          proj.serviceCenter ? proj.serviceCenter : '',
+          proj.stob ? proj.stob : '',
+          proj.projectCode ? proj.projectCode : '',
+          `$${proj.totalAmount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`,
         ]);
         console.log(tableRowsFormatted);
         const pdfSinglePageHeight = doc.internal.pageSize.height;
@@ -113,193 +117,159 @@ export default {
 
         console.log(tableHeaders);
         doc.setFontSize(12);
-        doc.text(
-          "Document # " + pdfValues[0].documentNo,
-          leftStartCoordinate,
-          20
-        );
-        doc.text("SAP", leftStartCoordinate + 150, 20);
-        doc.text("Line Description:", leftStartCoordinate, 30);
+        doc.text(`Document # ${pdfValues[0].documentNo}`, leftStartCoordinate, 20);
+        doc.text('SAP', leftStartCoordinate + 150, 20);
+        doc.text('Line Description:', leftStartCoordinate, 30);
         doc.setFontSize(18);
         doc.autoTable(tableHeaders, tableRowsFormatted, {
-          theme: "plain",
-          tableWidth: "auto",
+          theme: 'plain',
+          tableWidth: 'auto',
           margin: { top: 60 },
           styles: {
-            overflow: "linebreak",
+            overflow: 'linebreak',
             fontSize: 12,
-            overflowColumns: "linebreak"
-          }
+            overflowColumns: 'linebreak',
+          },
         });
         doc.setFontSize(11);
-        doc.setFontStyle("bold");
-        doc.text("Amount Check", leftStartCoordinate + 110, 100);
+        doc.setFontStyle('bold');
+        doc.text('Amount Check', leftStartCoordinate + 110, 100);
         doc.setFontSize(12);
-        doc.text("$0.00", leftStartCoordinate + 150, 100);
+        doc.text('$0.00', leftStartCoordinate + 150, 100);
 
-        /////////////// PDF First PAGE END /////////////////////////////////////////////
+        // ///////////// PDF First PAGE END /////////////////////////////////////////////
 
         for (let i = 0; i < pdfValues.length; i++) {
           doc.addPage();
-          doc.setFontStyle("normal");
+          doc.setFontStyle('normal');
           doc.setFontSize(11);
 
-          ///// PDF SECOND PAGE HEADER //////
+          // /// PDF SECOND PAGE HEADER //////
+          doc.text("Ministry of Citizens' Services", leftStartCoordinate + 50, 15);
           doc.text(
-            "Ministry of Citizens' Services",
-            leftStartCoordinate + 50,
-            15
-          );
-          doc.text(
-            "Procurement and Supply, Procurement Services Branch(Stategic and Advisory Services)",
+            'Procurement and Supply, Procurement Services Branch(Stategic and Advisory Services)',
             30,
-            21
+            21,
           );
-          doc.text("PO Box 9476 Stn Prov Govt", leftStartCoordinate + 50, 27);
-          doc.text("Victoria BC V8W 9W6", leftStartCoordinate + 55, 33);
+          doc.text('PO Box 9476 Stn Prov Govt', leftStartCoordinate + 50, 27);
+          doc.text('Victoria BC V8W 9W6', leftStartCoordinate + 55, 33);
 
-          ///// PDF SECOND PAGE HEADER END//////
+          // /// PDF SECOND PAGE HEADER END//////
 
           doc.text("Ministry of Citizens' Services", 15, 55);
-          doc.text("OCIO - Technology Soultions", 15, 61);
+          doc.text('OCIO - Technology Soultions', 15, 61);
 
-          doc.text(
-            pdfValues[i].dateCreated.toString().slice(0, 10),
-            leftStartCoordinate + 125,
-            55
-          );
+          doc.text(pdfValues[i].dateCreated.toString().slice(0, 10), leftStartCoordinate + 125, 55);
           doc.text(pdfValues[i].documentNo, leftStartCoordinate + 125, 61);
-          doc.text(pdfValues[i].clientNo, leftStartCoordinate + 125, 70);
-          doc.text(
-            pdfValues[i].responsibilityCenter,
-            leftStartCoordinate + 125,
-            76
-          );
-          doc.text(pdfValues[i].serviceCenter, leftStartCoordinate + 125, 82);
-          doc.text(pdfValues[i].stob, leftStartCoordinate + 125, 88);
-          doc.text(pdfValues[i].projectCode, leftStartCoordinate + 125, 94);
+          doc.text(pdfValues[i].clientNo ? pdfValues[i].clientNo : '', leftStartCoordinate + 125, 70);
+          doc.text(pdfValues[i].responsibilityCenter ? pdfValues[i].responsibilityCenter : '', leftStartCoordinate + 125, 76);
+          doc.text(pdfValues[i].serviceCenter ? pdfValues[i].serviceCenter : '', leftStartCoordinate + 125, 82);
+          doc.text(pdfValues[i].stob ? pdfValues[i].stob : '', leftStartCoordinate + 125, 88);
+          doc.text(pdfValues[i].projectCode ? pdfValues[i].projectCode : '', leftStartCoordinate + 125, 94);
 
           doc.setFontSize(12);
-          doc.setFontStyle("bold");
-          doc.text("Notification of Charges", leftStartCoordinate + 50, 105);
-          doc.setFontStyle("normal");
+          doc.setFontStyle('bold');
+          doc.text('Notification of Charges', leftStartCoordinate + 50, 105);
+          doc.setFontStyle('normal');
           doc.setFontSize(11);
           doc.text(pdfValues[i].projectName, 40, 115);
-          doc.text(pdfValues[i].reference, 40, 122);
-          doc.text(pdfValues[i].contact, 40, 129);
+          doc.text(pdfValues[i].reference ? pdfValues[i].reference : '', 40, 122);
+          doc.text(pdfValues[i].contact ? pdfValues[i].contact : '', 40, 129);
 
-          doc.setFontStyle("bold");
-          doc.text("Date", leftStartCoordinate + 110, 55);
-          doc.text("Document #", leftStartCoordinate + 97, 61);
-          doc.text("Client  ", leftStartCoordinate + 108, 70);
-          doc.text("Responsibility", leftStartCoordinate + 93, 76);
-          doc.text("ServiceLine", leftStartCoordinate + 97, 82);
-          doc.text("STOB", leftStartCoordinate + 107, 88);
-          doc.text("ProjectsCode", leftStartCoordinate + 93, 94);
-          doc.text("Program", 15, 115);
-          doc.text("Reference", 15, 122);
-          doc.text("Contact", 15, 129);
-          doc.text("Fees", leftStartCoordinate + 106, 160);
-          doc.text("Expenses", leftStartCoordinate + 106, 169);
-          doc.text("Total", leftStartCoordinate + 106, 178);
-          doc.text("Contact", 15, 200);
-          doc.text("Number of Pages", 15, 220);
+          doc.setFontStyle('bold');
+          doc.text('Date', leftStartCoordinate + 110, 55);
+          doc.text('Document #', leftStartCoordinate + 97, 61);
+          doc.text('Client  ', leftStartCoordinate + 108, 70);
+          doc.text('Responsibility', leftStartCoordinate + 93, 76);
+          doc.text('ServiceLine', leftStartCoordinate + 97, 82);
+          doc.text('STOB', leftStartCoordinate + 107, 88);
+          doc.text('ProjectsCode', leftStartCoordinate + 93, 94);
+          doc.text('Program', 15, 115);
+          doc.text('Reference', 15, 122);
+          doc.text('Contact', 15, 129);
+          doc.text('Fees', leftStartCoordinate + 106, 160);
+          doc.text('Expenses', leftStartCoordinate + 106, 169);
+          doc.text('Total', leftStartCoordinate + 106, 178);
+          doc.text('Contact', 15, 200);
+          doc.text('Number of Pages', 15, 220);
 
-          doc.setFontStyle("normal");
+          doc.setFontStyle('normal');
           doc.text(
-            "$" +
-              pdfValues[i].fees
-                .toString()
-                .replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+            `$${pdfValues[i].fees.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`,
             leftStartCoordinate + 136,
-            160
+            160,
           );
           doc.text(
-            "$" +
-              pdfValues[i].expenses
-                .toString()
-                .replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+            `$${pdfValues[i].expenses.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`,
             leftStartCoordinate + 136,
-            169
+            169,
           );
           doc.text(
-            "$" +
-              pdfValues[i].totalAmount
-                .toString()
-                .replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+            `$${pdfValues[i].totalAmount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`,
             leftStartCoordinate + 136,
-            178
+            178,
           );
 
-          doc.text(pdfValues[i].contact, leftStartCoordinate + 38, 200);
-          doc.text(
-            "Ministry of Citizens' Seevices",
-            leftStartCoordinate + 38,
-            205
-          );
+          doc.text(pdfValues[i].contact ? pdfValues[i].contact : '', leftStartCoordinate + 38, 200);
+          doc.text("Ministry of Citizens' Seevices", leftStartCoordinate + 38, 205);
 
           doc.text(
-            "Includes Time and Expenses up to " +
-              new Date(pdfValues[i].totalAmount).toString().slice(0, 15),
+            `Includes Time and Expenses up to ${
+              new Date(pdfValues[i].totalAmount).toString().slice(0, 15)}`,
             leftStartCoordinate + 35,
-            230
+            230,
           );
-          doc.setFontStyle("italic");
+          doc.setFontStyle('italic');
           doc.text(
-            "Processed by Inter-Ministry Electronic Chargeback System. Charges have already been made to ",
+            'Processed by Inter-Ministry Electronic Chargeback System. Charges have already been made to ',
             15,
-            260
+            260,
           );
           doc.text(
-            "your account and, unless you disagree with the charges no further action required by you",
+            'your account and, unless you disagree with the charges no further action required by you',
             15,
-            265
+            265,
           );
           doc.addPage();
-          doc.setFontStyle("normal");
-          doc.setFontStyle("bold");
-          doc.text("Billing Details", 15, 15);
-          doc.setFontStyle("normal");
+          doc.setFontStyle('normal');
+          doc.setFontStyle('bold');
+          doc.text('Billing Details', 15, 15);
+          doc.setFontStyle('normal');
           const tableBillingDetailsHeaders = [
-            "Date",
-            "Description",
-            "Type",
-            "Resource",
-            "Hours",
-            "Rate",
-            "Amount"
+            'Date',
+            'Description',
+            'Type',
+            'Resource',
+            'Hours',
+            'Rate',
+            'Amount',
           ];
-          const tableRowsBillingFormatted = pdfValues[
-            i
-          ].exportDetails.map(proj => [
+          const tableRowsBillingFormatted = pdfValues[i].details.map(proj => [
             proj.entryDate,
-            proj.description,
+            proj.description ? proj.description : '',
             proj.type,
-            proj.resource,
-            proj.hours ? proj.hours : "",
-            proj.rate ? proj.rate : "",
-            proj.amount
-              ? "$" +
-                proj.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-              : "$0"
+            proj.resource ? proj.resource : '',
+            proj.hours ? proj.hours : '',
+            proj.rate ? proj.rate : '',
+            proj.amount ? `$${proj.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}` : '$0',
           ]);
           doc.autoTable(tableBillingDetailsHeaders, tableRowsBillingFormatted, {
-            theme: "plain",
-            tableWidth: "auto",
+            theme: 'plain',
+            tableWidth: 'auto',
             margin: { top: 30 },
             styles: {
-              overflow: "linebreak",
+              overflow: 'linebreak',
               fontSize: 12,
-              overflowColumns: "linebreak"
-            }
+              overflowColumns: 'linebreak',
+            },
           });
 
           // theme: 'striped'|'grid'|'plain'|'css'
         }
-        doc.save("sample.pdf");
+        doc.save('sample.pdf');
       });
-    }
-  }
+    },
+  },
 };
 </script>
 <style>
