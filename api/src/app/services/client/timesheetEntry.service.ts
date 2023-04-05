67bc22c5fa9fe80a999244eed17309a61c23a52a
Josh-Williams-BC <josh.williams@gov.bc.ca>
2023-04-05 11:28:40 -0700

    adding transasction rollbacks for time entries

--- api/src/app/services/client/timesheetEntry.service.ts
+++ api/src/app/services/client/timesheetEntry.service.ts
@@ -2,6 +2,7 @@ import { getRepository, Repository } from 'typeorm';
 import { TimesheetEntry } from './../../models/entities/timesheetEntry.entity';
 import { ITimesheetEntry } from '../../models/interfaces/i-timesheet-entry';
 import { Timesheet } from './../../models/entities/timesheet.entity';
+import connection from '../../database/database.connection';
 
 const timesheetEntryRepo = (): Repository<TimesheetEntry> => {
   return getRepository(TimesheetEntry);
@@ -16,8 +17,23 @@ export const createTimesheetEntry = async (
 ) => {
   if (Array.isArray(obj)) {
     const arr: any = [];
-    for (let index = 0; index < obj.length; index++) {
-      arr.push(await createTimesheetEntry(obj[index]));
+    const queryRunner = (await connection).createQueryRunner();
+    const saveDate = new Date();
+    queryRunner.startTransaction();
+    try{
+      for (let index = 0; index < obj.length; index++) {
+        const timesheetEntry: TimesheetEntry = timesheetEntryRepo().create(obj[index]);
+        timesheetEntry.dateCreated = saveDate;
+        timesheetEntry.dateModified = saveDate;
+        await queryRunner.manager.save(timesheetEntry);
+      }
+    }catch(err){
+      console.log(err);
+      console.log("Rolling back transaction");
+      await queryRunner.rollbackTransaction();
+      throw(err);
+    }finally{
+      await queryRunner.release();
     }
     return arr;
   }
