375e55f2b0c95483e5c253637eac1e7538fbc88f
Josh-Williams-BC <114189319+Josh-Williams-BC@users.noreply.github.com>
2026-03-26 11:28:48 -0700

    Fiscal Year End Update to Timesheets.vue

--- web/src/components/timeMachine/timesheets/Timesheets.vue
+++ web/src/components/timeMachine/timesheets/Timesheets.vue
@@ -1,5 +1,14 @@
 <template>
   <v-container grid-list-xl fluid class="page-container custom-timesheets-container">
+        
+    <!-- Year-end notification -->
+    <div class="red white--text pa-3 mb-4" style="border-radius: 4px; margin-bottom: 6px !important; text-align: center;">
+      <strong>Important fiscal year-end notice:</strong>
+      Time for <strong>March 30 and March 31</strong> must be entered on timesheet
+      dates <strong>on or before March 29</strong>.
+      <br>Please complete all March entries by end of day <strong>April 7</strong>.
+    </div>
+    
     <v-layout>
       <v-flex md12>
         <h1 class="timesheet-header">Timesheets</h1>
